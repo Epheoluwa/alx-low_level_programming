292e3af5b663e80e33abde192bef731b6759b3e7
Solomon Sunmola <sunmolasolomon@gmail.com>
2022-06-02 07:54:31 -0700

    second task commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 
 
@@ -12,13 +12,11 @@
  *
  *     */
 
-
-
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
