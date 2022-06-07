85a6f0c82012dcc063a7c69175fa45d6773ccd61
Solomon Sunmola <sunmolasolomon@gmail.com>
2022-06-07 06:45:26 -0700

    first commit

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+The read me file for this project 
+C functions and loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/**
+ * this file is a header file that defines functions prototype for use
+ *
+ * Description: defines functions prototypes
+ *
+ * 
+ */
+
+void _putchar(char letter)
